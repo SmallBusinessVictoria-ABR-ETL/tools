febba1b9f30e56ae728c33cc0877f6699a93b3ff
jason <jason@portable.com.au>
2018-09-04 22:12:31 +1000

    gzip files before uploading

--- doAllTheThings.sh
+++ doAllTheThings.sh
@@ -22,16 +22,26 @@ aws s3 sync . s3://sbv-abr-etl/raw/${EXTRACT_DATE} --exclude "*" --include "*.tx
 diff-abr ../previous-combine.txt VIC${EXTRACT_DATE_SHORT}_ABR_Agency_Data.txt combined.txt ${DATE_PARTITION}
 
 # Replace agency data with new file -- TODO: convert to parquet
-aws s3 cp combined.txt s3://sbv-abr-etl/FACT/AGENCY/combined.txt
+gzip combined.txt
+aws s3 cp combined.txt.gz s3://sbv-abr-etl/FACT/AGENCY/combined.txt.gz
+
+# Compress files
+gzip OrgNameChange.txt
+gzip NameChange.txt
+gzip TradingNameChange.txt
+gzip SONAddressChange.txt
+gzip BusAddressChange.txt
+gzip EmailChange.txt
+gzip IndustryChange.txt
 
 # Add new change partitions
-aws s3 cp OrgNameChange.txt s3://sbv-abr-etl/DIMENSION/OrgNameChange/date=${DATE_PARTITION}/OrgNameChange.txt
-aws s3 cp NameChange.txt s3://sbv-abr-etl/DIMENSION/NameChange/date=${DATE_PARTITION}/NameChange.txt
-aws s3 cp TradingNameChange.txt s3://sbv-abr-etl/DIMENSION/TradingNameChange/date=${DATE_PARTITION}/TradingNameChange.txt
-aws s3 cp SONAddressChange.txt s3://sbv-abr-etl/DIMENSION/SONAddressChange/date=${DATE_PARTITION}/SONAddressChange.txt
-aws s3 cp BusAddressChange.txt s3://sbv-abr-etl/DIMENSION/BusAddressChange/date=${DATE_PARTITION}/BusAddressChange.txt
-aws s3 cp EmailChange.txt s3://sbv-abr-etl/DIMENSION/EmailChange/date=${DATE_PARTITION}/EmailChange.txt
-aws s3 cp IndustryChange.txt s3://sbv-abr-etl/DIMENSION/IndustryChange/date=${DATE_PARTITION}/IndustryChange.txt
+aws s3 cp OrgNameChange.txt.gz s3://sbv-abr-etl/DIMENSION/OrgNameChange/date=${DATE_PARTITION}/OrgNameChange.txt.gz
+aws s3 cp NameChange.txt.gz s3://sbv-abr-etl/DIMENSION/NameChange/date=${DATE_PARTITION}/NameChange.txt.gz
+aws s3 cp TradingNameChange.txt.gz s3://sbv-abr-etl/DIMENSION/TradingNameChange/date=${DATE_PARTITION}/TradingNameChange.txt.gz
+aws s3 cp SONAddressChange.txt.gz s3://sbv-abr-etl/DIMENSION/SONAddressChange/date=${DATE_PARTITION}/SONAddressChange.txt.gz
+aws s3 cp BusAddressChange.txt.gz s3://sbv-abr-etl/DIMENSION/BusAddressChange/date=${DATE_PARTITION}/BusAddressChange.txt.gz
+aws s3 cp EmailChange.txt.gz s3://sbv-abr-etl/DIMENSION/EmailChange/date=${DATE_PARTITION}/EmailChange.txt.gz
+aws s3 cp IndustryChange.txt.gz s3://sbv-abr-etl/DIMENSION/IndustryChange/date=${DATE_PARTITION}/IndustryChange.txt.gz
 
 # Copy current combined into parent folder for next run
 cp combined.txt ../previous-combine.txt
